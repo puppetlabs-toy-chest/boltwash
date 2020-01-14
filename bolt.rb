04f8c10f5c20079b388b88b724d2a574bb758415
Michael Smith <michael.smith@puppet.com>
2020-01-14 12:20:43 -0800

    Use prefetch to list all groups/targets up-front
    Allows us to avoid serializing Bolt::Inventory.
    
    Signed-off-by: Michael Smith <michael.smith@puppet.com>

--- bolt.rb
+++ bolt.rb
@@ -6,62 +6,48 @@ require 'bolt/inventory'
 # TODO: should be required by bolt/config, where it's used.
 require 'bolt/logger'
 
-def setup_inventory(inventory)
-  if inventory[:config]
-    inventory = JSON.parse(inventory.to_json)
-  end
-
-  config = Bolt::Config.default
-  config.overwrite_transport_data(inventory['config']['transport'],
-                                  Bolt::Util.symbolize_top_level_keys(inventory['config']['transports']))
-
-  Bolt::Inventory.new(inventory['data'],
-                      config,
-                      Bolt::Util.symbolize_top_level_keys(inventory['target_hash']))
-end
-
 class Boltwash < Wash::Entry
   label 'bolt'
   is_singleton
   parent_of 'Group'
-  state :inventory_data
 
   def init(config)
     boltdir = config['dir']
     boltdir ||= Bolt::Boltdir.default_boltdir
 
     bolt_config = Bolt::Config.from_boltdir(boltdir)
-    @inventory_data = Bolt::Inventory.from_config(bolt_config).data_hash
+    @inventory = Bolt::Inventory.from_config(bolt_config)
+    prefetch :list
   end
 
   def list
-    groups = setup_inventory(@inventory_data).group_names
-    groups.map { |group| Group.new(@inventory_data, group) }
+    groups = @inventory.group_names
+    groups.map { |group| Group.new(@inventory, group) }
   end
 end
 
 class Group < Wash::Entry
   label 'group'
   parent_of 'Target'
-  state :inventory_data
 
-  def initialize(data, name)
-    @inventory_data = data
+  def initialize(inventory, name)
+    @inventory = inventory
     @name = name
+    prefetch :list
   end
 
   def list
-    group = setup_inventory(@inventory_data).instance_variable_get(:@group_lookup)[@name]
-    group.nodes.keys.map { |target| Target.new(@inventory_data, target)}
+    # TODO: expose a way to get group names
+    group = @inventory.instance_variable_get(:@group_lookup)[@name]
+    group.nodes.keys.map { |target| Target.new(@inventory, target)}
   end
 end
 
 class Target < Wash::Entry
   label 'target'
-  state :inventory_data
 
-  def initialize(data, name)
-    @inventory_data = data
+  def initialize(inventory, name)
+    # TODO: get target details from inventory and save as state
     @name = name
   end
 end
