81c3ce04506b99a813c6afa5e01efafb2feac173
Michael Smith <michael.smith@puppet.com>
2020-02-03 17:15:44 -0800

    Fix handling stdin
    The switch to using Bolt transport connections broke handling stdin.
    This fixes it.
    
    Signed-off-by: Michael Smith <michael.smith@puppet.com>

--- transport_ssh.rb
+++ transport_ssh.rb
@@ -1,8 +1,6 @@
 require 'bolt/transport/base'
 require 'bolt/transport/sudoable'
 require 'bolt/transport/ssh/connection'
-require 'concurrent/atomic/atomic_reference'
-require 'concurrent/promises'
 
 class BoltSSH < Bolt::Transport::SSH::Connection
   # Adapted from Bolt::Transport::SSH::Connection.execute with output copied to
@@ -10,8 +8,13 @@ class BoltSSH < Bolt::Transport::SSH::Connection
   # handles stdin differently with sudo; it waits for some response, then
   # sends stdin.
   def execute(command, stdin: nil)
+    raise 'stdin not supported while using tty' if stdin && target.options['tty']
+
+    # If not nil, stdin==STDIN. Read all input so we have a string to pass around.
+    stdin = stdin.read if stdin
+
     escalate = run_as && @user != run_as
-    use_sudo = escalate && @target.options['run-as-command'].nil?
+    use_sudo = escalate && target.options['run-as-command'].nil?
 
     if escalate
       if use_sudo
@@ -19,7 +22,7 @@ class BoltSSH < Bolt::Transport::SSH::Connection
         sudo_flags = [sudo_exec, "-S", "-H", "-u", run_as, "-p", Bolt::Transport::Sudoable.sudo_prompt]
         sudo_str = Shellwords.shelljoin(sudo_flags)
       else
-        sudo_str = Shellwords.shelljoin(@target.options['run-as-command'] + [run_as])
+        sudo_str = Shellwords.shelljoin(target.options['run-as-command'] + [run_as])
       end
       command = build_sudoable_command_str(command, sudo_str, @sudo_id, stdin: stdin, reset_cwd: true)
     end
@@ -37,17 +40,11 @@ class BoltSSH < Bolt::Transport::SSH::Connection
           )
         end
 
-        received = Concurrent::Promises.resolvable_future
-        # Automatically resolve it if we're not using sudo so we immediately send input
-        received.fulfill(true, false) unless use_sudo
-
         channel.on_data do |_, data|
-          received.fulfill(true, false)
           $stdout << data unless use_sudo && handled_sudo(channel, data, stdin)
         end
 
         channel.on_extended_data do |_, _, data|
-          received.fulfill(true, false)
           $stderr << data unless use_sudo && handled_sudo(channel, data, stdin)
         end
 
@@ -55,9 +52,8 @@ class BoltSSH < Bolt::Transport::SSH::Connection
           exit_code = data.read_long
         end
 
-        # Wait until we know sudo is handled, then send stdin.
-        if stdin && received.value!
-          channel.send_data(options[:stdin])
+        if stdin && !use_sudo
+          channel.send_data(stdin)
           channel.eof!
         end
       end
